92d692517cbe838edc2e997f79895f1be76ad924
Gia-My Nguyen <my.ngngia@gmail.com>
2026-01-28 23:22:54 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { LoadingScreen } from './assets/loadingScreen'
+import './index.css'
+function App() {
+
+
+  return (
+    <>
+      <LoadingScreen />
+    </>
+  )
+}
+
+export default App
